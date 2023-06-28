4531eabb68edd7b42ea0802fc035c7b20c38b1f3
JinHo Hong <ghddhksduq@naver.com>
2023-06-29 00:34:00 +0900

    BreakObject_HJH추가

---
+++ EvenI/Assets/HJH/Script/BreakObject_HJH.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BreakObject_HJH : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "Player")
+        {
+            collision.gameObject.GetComponent<Player_shj>().hp--;
+            Destroy(gameObject);
+        }
+    }
+}
