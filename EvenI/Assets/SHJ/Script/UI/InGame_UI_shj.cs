61333f612f2c618bec1c8e0741f7889464706f07
NFUE2 <96811655+NFUE2@users.noreply.github.com>
2023-07-06 14:35:21 +0900

    대나무 및 트랩 추가,플레이어 프리펩생성
    대나무 및 트랩 추가,플레이어 프리펩생성

--- EvenI/Assets/SHJ/Script/UI/InGame_UI_shj.cs
+++ EvenI/Assets/SHJ/Script/UI/InGame_UI_shj.cs
@@ -2,16 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class InGame_UI_shj : UI_Setting_shj
 {
-    public Text count_text;
+    public TextMeshProUGUI count_text;
+
+    public GameObject HP;
+    public Transform HP_list;
 
     int count = 0;
+
+    public int Count { get { return count; } set { count = value; } }
+
+    private void Start()
+    {
+        if(HP_list.childCount == 0)
+        {
+            for (int i = 0; i < GameObject.Find("Player").GetComponent<Player_shj>().hp; i++)
+            {
+                GameObject obj = Instantiate(HP);
+                obj.transform.parent = HP_list;
+            }
+        }
+    }
 
     private void Update()
     {
         count_text.text = count.ToString();
+        Debug.Log(count.ToString());
     }
 
     public void Game_Stop() //게임 정지 버튼
