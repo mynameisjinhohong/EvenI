07c2bdf961043e122138f64feae579385c28afff
JinHo Hong <ghddhksduq@naver.com>
2023-07-06 14:53:18 +0900

    Revert "맵 수정"
    This reverts commit 60eac4cb2f8dee0d5cac8e9d7a403bc27dff189c, reversing
    changes made to b6283315966c8178df00b2dcb81098987585e83d.

--- EvenI/Assets/HJH/Script/JukSun_HJH.cs
+++ EvenI/Assets/HJH/Script/JukSun_HJH.cs
@@ -31,7 +31,5 @@ public class JukSun_HJH : Object_Manager_shj
     public override void Item_Active(GameObject player)
     {
         //UI연동해서 스코어 올릴수있는 스크립트 작성해야함
-        GameObject.Find("InGame_UI").GetComponent<InGame_UI_shj>().Count++;
-        Debug.Log(GameObject.Find("InGame_UI").GetComponent<InGame_UI_shj>().Count);
     }
 }
