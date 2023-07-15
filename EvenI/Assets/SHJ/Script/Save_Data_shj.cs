4e73d3e4632a51c76a9cac0e46f7d4ee1b856aa3
NFUE2 <96811655+NFUE2@users.noreply.github.com>
2023-07-15 11:55:28 +0900

    데이터 저장기능 및 ui일부수정
    데이터 저장기능 및 ui일부수정

---
+++ EvenI/Assets/SHJ/Script/Save_Data_shj.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Save_Data_shj : MonoBehaviour
+{
+    public string nickname =  "Å×½ºÆ®";
+    public int last_play_scene_num = 1;
+
+
+    //public Save_Data_shj()
+    //{
+    //    nickname = "";
+    //    last_play_scene_num = -1;
+    //}
+}
