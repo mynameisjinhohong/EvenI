6a58523c08e3a8384f893e9d6ad83c066e91015a
JinHo Hong <ghddhksduq@naver.com>
2023-07-25 22:12:28 +0900

    설원 장애물 끝

--- EvenI/Assets/HJH/Script/IceBreakFloor_HJH.cs
+++ EvenI/Assets/HJH/Script/IceBreakFloor_HJH.cs
@@ -4,13 +4,24 @@ using UnityEngine;
 
 public class IceBreakFloor_HJH : Object_Manager_shj
 {
+    public float aniSpeed;
+    bool touch = false;
     public override void Item_Active(GameObject player)
     {
         ItmeInActive = false;
+        animator.SetFloat("Speed", aniSpeed);
         if (animator != null)
         {
             animator.SetTrigger("Touch");
         }
-        Debug.Log("gogo");
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player" && !touch)
+        {
+            Active(collision.gameObject);
+            touch = true;
+        }
     }
 }
