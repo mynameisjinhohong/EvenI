dc5e20448225e921073c80c43c9d72271c5094d8
JinHo Hong <ghddhksduq@naver.com>
2023-07-26 20:41:11 +0900

    맵 스크롤 버그 수정

--- EvenI/Assets/HJH/Script/MapScrolling_HJH.cs
+++ EvenI/Assets/HJH/Script/MapScrolling_HJH.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class MapScrolling_HJH : MonoBehaviour
@@ -7,6 +5,7 @@ public class MapScrolling_HJH : MonoBehaviour
     [Range(0.0f, 10.0f)]
     public float speed;
     public GameObject player;
+    Player_shj play;
     public GameObject[] bgs;
     public GameObject[] bgParents;
     public float distance;
@@ -18,39 +17,47 @@ public class MapScrolling_HJH : MonoBehaviour
         player = GameObject.Find("Player");
         bgSize = GetBGSize(bgs[0]);
         Debug.Log(bgSize);
+        play = player.GetComponent<Player_shj>();
         nowBg = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position += Vector3.left * speed * Time.deltaTime;
-        if (bgParents .Length >1)
+        if (!play.gameClear)
         {
-            if (player.transform.position.x > (bgParents[nowBg].transform.position + bgSize / 2).x + distance)
-            {
-                bgParents[nowBg].transform.position = new Vector3(bgParents[nowBg].transform.position.x + (bgSize.x * 4), bgParents[nowBg].transform.position.y, 0);
-                nowBg++;
-                if (nowBg > 3)
-                {
-                    nowBg = 0;
-                }
-            }
+            transform.position += Vector3.left * speed * Time.deltaTime;
         }
-        else
+        if (!play.gameClear)
         {
-            if (player.transform.position.x > (bgs[nowBg].transform.position + bgSize / 2).x + distance)
+            if (bgParents.Length > 1)
             {
-                bgs[nowBg].transform.position = new Vector3(bgs[nowBg].transform.position.x + (bgSize.x * 4), bgs[nowBg].transform.position.y, 0);
-                nowBg++;
-                if (nowBg > 3)
+                if (player.transform.position.x > (bgParents[nowBg].transform.position + bgSize / 2).x + distance)
                 {
-                    nowBg = 0;
+                    bgParents[nowBg].transform.position = new Vector3(bgParents[nowBg].transform.position.x + (bgSize.x * 4), bgParents[nowBg].transform.position.y, 0);
+                    nowBg++;
+                    if (nowBg > 3)
+                    {
+                        nowBg = 0;
+                    }
                 }
             }
+            else
+            {
+                if (player.transform.position.x > (bgs[nowBg].transform.position + bgSize / 2).x + distance)
+                {
+                    bgs[nowBg].transform.position = new Vector3(bgs[nowBg].transform.position.x + (bgSize.x * 4), bgs[nowBg].transform.position.y, 0);
+                    nowBg++;
+                    if (nowBg > 3)
+                    {
+                        nowBg = 0;
+                    }
+                }
 
+            }
         }
-       
+
+
     }
     public Vector3 GetBGSize(GameObject bG)
     {
