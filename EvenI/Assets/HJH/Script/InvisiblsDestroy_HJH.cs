dd90cc3820a3ea84fa5527b0031ac6f6408d7343
JinHo Hong <ghddhksduq@naver.com>
2023-07-27 01:40:30 +0900

    최적화 수정

--- EvenI/Assets/HJH/Script/InvisiblsDestroy_HJH.cs
+++ EvenI/Assets/HJH/Script/InvisiblsDestroy_HJH.cs
@@ -6,6 +6,11 @@ public class InvisiblsDestroy_HJH : MonoBehaviour
 {
     private void OnBecameInvisible()
     {
-        gameObject.SetActive(false);
+        Camera cam = Camera.main;
+        Vector3 viewPos = cam.WorldToViewportPoint(transform.position);
+        if (viewPos.x <0)
+        {
+            gameObject.SetActive(false);
+        }
     }
 }
